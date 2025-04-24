c1704955bb729484e9e4ec15f7b0a5153ad2a7f3
Ibrahim Megahed <pnpop01027066161@gmail.com>
2025-04-24 23:56:52 +0200

    fix: enhance redis Dockerfile and entrypoint script for improved setup and execution

--- src/redis/entrypoint.sh
+++ src/redis/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+. /app-ci/entrypoint.sh echo ""
+
+# Prepare software
 if [ -n "$REDIS_PASSWORD" ]; then
   export REDIS_ARGS="--requirepass $REDIS_PASSWORD"
 fi
 
+# Run current
 if [ $# -eq 0 ]; then
   su-exec redis redis-server --save 60 1 --loglevel notice --dir /data $REDIS_ARGS
 fi
 
+# Run next
 exec "$@"
