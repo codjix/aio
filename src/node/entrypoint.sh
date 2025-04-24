c6a2942d9f1d9aab8db144e651112e86eae8a544
Ibrahim Megahed <pnpop01027066161@gmail.com>
2025-04-24 23:55:13 +0200

    fix: enhance node Dockerfile and entrypoint script for improved setup and execution

--- src/node/entrypoint.sh
+++ src/node/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
-  set -- node
+. /app-ci/entrypoint.sh echo ""
+
+# Run current
+if [ $# -eq 0 ]; then
+  node
 fi
 
+# Run next
 exec "$@"
