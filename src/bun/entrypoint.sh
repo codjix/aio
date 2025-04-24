e27d548afb299f1a076854e6ca461704e3344a33
Ibrahim Megahed <pnpop01027066161@gmail.com>
2025-04-24 23:25:56 +0200

    fix: enhance bun Dockerfile and entrypoint script for improved setup and execution

--- src/bun/entrypoint.sh
+++ src/bun/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+. /app-ci/entrypoint.sh echo ""
+
+# Prepare software
 if [ -f /app-ci/bun.tar.xz ]; then
   tar xf /app-ci/bun.tar.xz -C $BUN_INSTALL_BIN
   ln -s $BUN_INSTALL_BIN/bun $BUN_INSTALL_BIN/bunx
   rm -rf /app-ci/bun.tar.xz
 fi
 
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
-  set -- bun repl
+# Run current
+if [ $# -eq 0 ]; then
+  bun repl
 fi
 
+# Run next
 exec "$@"
